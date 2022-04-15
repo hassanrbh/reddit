8e85defba59cfad82b1d5f25135442dc0c447672
hassantarif <hassantarif4@gmail.com>
2022-04-15 13:12:29 +0000

    adding styles to the button

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,13 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'pulsate': 'pulsate 1s ease-in-out',
+        'pulsate-slow': 'pulsate-slow 1s ease-in-out',
+      }
+    }
+  }
 }
