b8e9e28875a0792cdad27881997a7707879df2bd
hassantarif <hassantarif4@gmail.com>
2022-04-15 14:49:45 +0000

    adding subs controller the 7 actions

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,21 +1,74 @@
 class SubsController < ApplicationController
     before_action :authenticate_user!
+    before_action :clarify_user!, only: [:edit, :update]
 
     def index
-        @subs = Sub.all
+        if !params[:q].present?
+            @subs = Sub.all
+        else
+            @subs = Sub.where("title LIKE '%#{sanitize_sql(params[:q])}%'")
+        end
     end
 
     def new
+        @sub = Sub.new
+        render :new
     end
     
+    def show
+        @sub = Sub.find_by(:id => params[:id])
+    end
     def create
+        @sub = Sub.new(subs_params)
+        @sub.moderator_id = current_user.id
+
+        if @sub.save
+            UserMailer.with(user: current_user,sub: @sub).notify_sub.deliver_now
+            flash[:notice] = "Sub Created Successfully 🎒"
+            redirect_to sub_path(@sub.id)
+        else
+            # UserMailer.with(user: current_user,sub: @sub).notify_sub_error.deliver_now
+            flash[:error] = "Failed to Create Sub 🔫"
+            render :new
+        end
     end
 
     def edit
+        @sub = Sub.find_by(:id => params[:id])
+        render :edit
     end
 
     def update
+        @sub = Sub.find_by(:id => params[:id])
+
+        if @sub.update(subs_params_update)
+            # UserMailer.with(user: current_user,sub: @sub).notify_sub_update.deliver_now
+            flash[:notice] = "Sub Updated Successfully 👽🔪"
+            redirect_to sub_path(@sub.id)
+        else
+            # UserMailer.with(user: current_user,sub: @sub).notify_sub_update_error.deliver_now
+            flash[:error] = "Failed To Update Sub 🧨"
+            render :edit
+        end
     end
 
     private
+
+    def subs_params
+        params.require(:subs).permit(:title,
+            :description,
+            :moderator_id
+        )
+    end
+
+    def subs_params_update
+        params.require(:subs).permit(
+            :title,
+            :description
+        )
+    end
+    def clarify_user!
+        return if current_user.subs.find_by(:id => params[:id])
+        render(:file => File.join(Rails.root,'public/404.html'),:status => 404, :layout => false)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  default_url_options :host => 'localhost', :port => 3000
+  
   root to: "subs#index"
+
   devise_scope :user do
     # redirect signing out users back to sign-in
     get "login", to: "devise/sessions#new"
     get "register", to: "devise/registrations#new"
   end
+
   devise_for :users
   resources :subs
 end
